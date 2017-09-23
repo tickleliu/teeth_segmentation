0d2e0beae7902f040f2dd0376d6f8a2a2a2e2219
xiaolubambi <liuminglu19870419@163.com>
2017-09-23 09:49:40 +0800

    增加了牙齿连通域h识别算法

--- calc_edge_main.m
+++ calc_edge_main.m
@@ -4,9 +4,12 @@ close all; clear all; clc;
 
 %% load files
 [ mFiles] = RangTraversal( './fig', 'fig' );
-sample_count = 1;
+sample_count = 5;
 sample_index = randperm(length(mFiles), sample_count);% random select several files
-% sample_index = 292;
+% sample_index = 234;
+% sample_index = 433;
+% sample_index = 447;
+% sample_index = 45;
 sample_images = {};
 for i = 1 : sample_count
     sample_images(i).image = loadfig(cell2mat(mFiles(sample_index(i))));  
@@ -30,19 +33,60 @@ for i = 1 : sample_count
 %     bw3 = imerode(bw3, B);imdilate
     subplot(sample_count, 2, i * 2 - 1);
     bw3 = sample(bw3, image_out);
-    imagesc(bw3);
-    subplot(sample_count, 2, i * 2);  
     bw3 = link_gap(bw3, 5);
-    
     bw3 = ~bw3;
-    L = bwlabel(bw3,4);    
+    L = bwlabel(bw3,4);  
+    
     S = regionprops(L, 'area');
-    L2 = ismember(L, find([S.Area] >= 200));
+    L2 = ismember(L, find([S.Area] >= 20));
     L_s = bwlabel(L2,4);
-    L_s = interest_region(L_s, image_out);
-    L = render_link_image(L_s);
     L_out =  mask(image_out);
-    L = L .* L_out;
-    L(:,:,1) = L(:,:,1) + image_out; 
+    L_s = L_s .* L_out;
+%     img_L = render_link_image(L_s); 
+%     img_L(:,:,1) = img_L(:,:,1) + image_out; 
+    imagesc(L_s + image_out);
+    subplot(sample_count, 2, i * 2);  
+   
+    L_s = interest_region(L_s, image_out);
+    downborder = zeros(1, size(L_s, 2));
+    
+    x = [];
+    y = [];
+    %find down border for each region
+    start_x = 10000;
+    end_x = 0;
+    for j = 1 : length(downborder)
+        teeth_pixel = find(L_s(:, j) ~= 0);
+        if ~isempty(teeth_pixel)
+            if start_x > j
+                start_x = j;
+            end
+            
+            if j > end_x
+                end_x = j;
+            end
+            downborder(j) = max(teeth_pixel);
+%             L_s(downborder(j), j) = 100;
+            x = [x, j];
+            y = [y, downborder(j)];
+        end
+    end
+    
+    yi = spline(x, y, [1 : length(downborder)]);
+    yi(floor(yi) <= 0) = 1;
+    yi(yi > size(L,1)) = size(L,1);
+    
+    L = render_link_image(L_s); 
+%     L(:,:,1) = L(:,:,1) + image_out; 
+    L(:,:,1) = L(:,:,1);
+    for j = start_x : end_x
+        L(floor(yi(j)), j, 1) = L(floor(yi(j)), j ,1) + 10;
+    end
+    temp_image = sample_images(i).image;
+    for j = start_x : end_x
+        s = min(find(image_out(:, j)~=0));    
+        temp_image( s : floor(yi(j)), j) = 50;
+    end
+%     image(temp_image);
     imagesc(L);
 end
--- interest_region.m
+++ interest_region.m
@@ -1,6 +1,12 @@
 function L_s = interest_region(L, image_out)
 % select the region near the image outer
+% select the region most like teeth
+% and rode the region near upper border to seperate
+% L must be a labeled linked region index from 1 to N
+% image_outer is the range teeth border labeled 1
+
 S = regionprops(L, 'area');
+Box = regionprops(L, 'boundingbox');
 region_prob = zeros(1, length([S.Area]));
 borderheight = zeros(1, size(image_out,2));
 region_inter =  zeros(length([S.Area]), length([S.Area]));
@@ -14,6 +20,11 @@ for i  = 1 : length(borderheight)
     cur_region = 0;
     for j = borderheight(i) : size(L,1)
         if L(j, i) ~= 0 && cur_region == 0 % interest region must near the border
+            
+            if j - borderheight(i) > 20 % the current region too far away from the border, give up this i
+                break
+            end
+            
             region_prob(L(j, i)) = region_prob(L(j, i)) ...
                 + 1;
             cur_region = L(j, i);
@@ -21,32 +32,160 @@ for i  = 1 : length(borderheight)
         
         if cur_region ~= L(j, i) && L(j, i)~= 0 % find two layer deep region
             region_inter(L(j, i), cur_region) = region_inter(L(j, i), cur_region) + 1;
-            break;
+            cur_region = L(j, i);
+            if j - borderheight(i) < 20 % the current region too far away from the border, give up this i
+                region_prob(L(j, i)) = region_prob(L(j, i)) ...
+                + 1;
+            end
         end
     end
 end
-region_prob(region_prob < 5) = 0;
+bound = reshape([Box.BoundingBox], 4, length([S.Area]));
+bound = bound(3,:);
+
+for i = 1 : length(region_prob)
+    if region_prob(i) > 30
+        continue;
+    end
+    
+    if region_prob(i) / bound(i) < 0.5
+        region_prob(i) = 0;
+    end
+%     region_prob(region_prob < 5) = 0;%delete the region far away from border
+end
+
+
 region_need_r = [];
+region_need_s = [];
 
 for i = 1 : length(region_prob)
    if  region_prob(i) == 0
        continue
    end
-   if sum(region_inter(i, :)) > 10
+   if sum(region_inter(i, :)) > 50
        region_need_r = [region_need_r, i];
+   else
+       region_need_s = [region_need_s, i];
    end   
 end
 
-L_t = zeros(size(L));
-
+L_temp = zeros(size(L));
 for i = 1 : length(region_need_r)
-    L_t(L == region_need_r(i)) = region_need_r(i);
+    L_temp(L == region_need_r(i)) = region_need_r(i);
 end
-figure(1)
-img = render_link_image(L_t);
-imagesc(img);
-figure(2)
-img2 = render_link_image(L);
-imagesc(img2);
-L_s = L;
+L_s = zeros(size(L));
+for i = 1 : length(region_need_s)
+    L_s(L == region_need_s(i)) = region_need_s(i);
+end
 
+for i = 1 : 50
+    [L_s, L_temp, region_need_r] = i_interest_region(L_s, L_temp, image_out, i);
+    if isempty(region_need_r)
+        break
+    end
+end
+
+% figure(1)
+% img = render_link_image(L);
+% imagesc(img);
+% figure(2)
+% img2 = render_link_image(L_s);
+% imagesc(img2);
+
+
+end
+
+function [L_s, L_temp, region_need_r] = i_interest_region(L_s, L_temp, image_out, rode_count)
+
+B = [0 1 0; 1 1 1; 0 1 0];
+L_temp = imerode(L_temp, B);
+L_temp = bwlabel(L_temp, 4);
+r_region_start = max(max(L_s));
+L_temp = L_temp + r_region_start;
+L_temp(L_temp == r_region_start) = 0;
+L = L_temp + L_s;
+
+%     bw3 = imerode(bw3, B);imdilate
+S = regionprops(L, 'area');
+Box = regionprops(L, 'boundingbox');
+region_prob = zeros(1, length([S.Area]));
+borderheight = zeros(1, size(image_out,2));
+region_inter =  zeros(length([S.Area]), length([S.Area]));
+for i  = 1 : length(borderheight)
+    border_i = find(image_out(:,i)~=0);
+    if isempty(border_i)
+        continue;
+    end
+    borderheight(i) = min(border_i);
+    
+    cur_region = 0;
+    for j = borderheight(i) : size(L,1)
+        if L(j, i) ~= 0 && cur_region == 0 % interest region must near the border  
+            if j - borderheight(i) > 20 % the current region too far away from the border, give up this i
+                break
+            end    
+            region_prob(L(j, i)) = region_prob(L(j, i)) ...
+                + 1;
+            cur_region = L(j, i);
+        end
+        
+        if cur_region ~= L(j, i) && L(j, i)~= 0 % find two layer deep region
+            region_inter(L(j, i), cur_region) = region_inter(L(j, i), cur_region) + 1;
+            cur_region = L(j, i);
+            if j - borderheight(i) < 20 % the current region too far away from the border, give up this i
+                region_prob(L(j, i)) = region_prob(L(j, i)) ...
+                + 1;
+            end
+        end
+        
+    end
+end
+
+bound = reshape([Box.BoundingBox], 4, length([S.Area]));
+bound = bound(3,:);
+for i = 1 : length(region_prob)
+    if region_prob(i) > 30
+        continue;
+    end
+    if region_prob(i) / bound(i) < 0.5
+        region_prob(i) = 0;
+    end
+%     region_prob(region_prob < 5) = 0;%delete the region far away from border
+end
+region_need_r = [];
+region_need_s = [];
+
+for i = 1 : length(region_prob)
+   if  region_prob(i) == 0
+       continue
+   end
+   if sum(region_inter(i, :)) > 50
+       region_need_r = [region_need_r, i];
+   else
+       region_need_s = [region_need_s, i];
+   end   
+end
+
+L_temp = zeros(size(L));
+for i = 1 : length(region_need_r)
+    L_temp(L == region_need_r(i)) = region_need_r(i);
+end
+for i = 1 : length(region_need_s)
+    L_s(L == region_need_s(i)) = region_need_s(i);  
+    if region_need_s(i) > r_region_start
+        for kk = 1 : rode_count
+            L_s = dilate_region_by_index(L_s, region_need_s(i));
+        end
+    end
+end
+end
+
+function L_out = dilate_region_by_index(L, index)
+% dilate the index region
+L_temp = zeros(size(L));
+L_temp(L == index) = index;
+L(L == index) = 0;
+B = [0 1 0; 1 1 1; 0 1 0];
+L_temp = imdilate(L_temp, B);
+L_out = L + L_temp;
+end
